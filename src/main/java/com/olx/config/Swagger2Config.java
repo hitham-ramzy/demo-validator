0a871f089ccdbb1eb8b2a95fe78c2ef262ccd1a4
Hitham Ramzy <hitham.ramzy@mondiamedia.com>
2019-03-31 19:59:30 +0200

    specify resource package to be in swagger

--- src/main/java/com/olx/config/Swagger2Config.java
+++ src/main/java/com/olx/config/Swagger2Config.java
@@ -17,7 +17,7 @@ public class Swagger2Config {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.olx.resources"))
                 .paths(PathSelectors.any())
                 .build();
     }
