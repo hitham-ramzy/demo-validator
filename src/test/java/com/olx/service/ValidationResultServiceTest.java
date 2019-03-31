5749f640a30cf67fa37e2dbd7cf3a73e034e7528
Hitham Ramzy <hitham.ramzy@mondiamedia.com>
2019-03-31 23:45:18 +0200

    running processed file test cases sequential

--- src/test/java/com/olx/service/ValidationResultServiceTest.java
+++ src/test/java/com/olx/service/ValidationResultServiceTest.java
@@ -2,8 +2,10 @@ package com.olx.service;
 
 import com.olx.OlxApplication;
 import com.olx.model.ProcessedFile;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,33 +22,37 @@ import static org.junit.Assert.assertNull;
         classes = OlxApplication.class)
 @AutoConfigureMockMvc
 @TestPropertySource(locations = "classpath:application-test.properties")
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ValidationResultServiceTest {
 
     @Autowired
     ProcessedFileService processedFileService;
 
+    private static Long fileId;
+
     @Test
-    public void a_save() {
+    public void test1_save() {
         ProcessedFile processedFile = processedFileService.save("FILE_NAME");
+        fileId = processedFile.getFileId();
         assertNotNull(processedFile.getFileId());
     }
 
     @Test
-    public void b_findByFileId() {
-        ProcessedFile processedFile = processedFileService.findByFileId(1L);
+    public void test2_findByFileId() {
+        ProcessedFile processedFile = processedFileService.findByFileId(fileId);
         assertNotNull(processedFile.getFileId());
     }
 
     @Test
-    public void c_getLatest() {
+    public void test3_getLatest() {
         ProcessedFile processedFile = processedFileService.getLatest();
-        assertEquals(new Long(1), processedFile.getFileId());
+        assertEquals(new Long(fileId), processedFile.getFileId());
     }
 
     @Test
-    public void d_findByFileId_notFound() {
+    public void test4_findByFileId_notFound() {
         ProcessedFile processedFile = processedFileService.findByFileId(100L);
-        assertNull(processedFile.getFileId());
+        assertNull(processedFile);
     }
 
 }
